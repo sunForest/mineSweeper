92601f20275026b4849b8caef4ec8fa33d183d3b
sunForest <sensun@live.cn>
2016-09-06 19:26:00 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+
+var BUILD_DIR = path.resolve(__dirname + '/build');
+var APP_DIR = path.resolve(__dirname + '/app');
+
+var config = {
+    entry: APP_DIR + 'index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    devtool: 'source-map',
+    devServer: {
+        inline: true,
+        contentBase: BUILD_DIR,
+        port: 3000
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx/,
+            include: APP_DIR,
+            loader: 'babel',
+            query: ['es2015', 'react']
+        }]
+    }
+};
+
+module.exports = config;
